fab444ed2d283b32c10ad473876b6b6258ffdb2b
Muskan-771 <muskanvij771@gmail.com>
2025-06-06 02:14:47 +0530

    update app.js get

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
     useTempFiles: true,
     tempFileDir: "/tmp/",
   })
-);
+); 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
